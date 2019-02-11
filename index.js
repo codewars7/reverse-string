f64df0cd40004dec5f176f2f34125f916a871d0e
americanman <snowboardracer7@gmail.com>
2019-02-11 15:26:40 -0500

    Add string reverse with for loop

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ let reverseStringSplit = (str) => {
 }
 
 reverseStringSplit("bye");
+
+let reverseStringForLoop = (str) => {
+  let newStr = '';
+  for( let i = str.length - 1; i >= 0; i --) {
+    newStr += str[i]
+  }
+  console.log(newStr)
+}
+
+reverseStringForLoop('!oG')
