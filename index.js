cf8afb487d345282586a82acebcb8410cdd7c92c
americanman <snowboardracer7@gmail.com>
2019-02-11 15:17:05 -0500

    Add reverse with spread operator

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// reverse with spread operator
 let reverseString = (str) => {
 // reverse string with reverse and join method
 // first we should convert it to an array
@@ -6,3 +7,13 @@ let reverseString = (str) => {
 }
 
 reverseString("hello");
+
+// reverse with split
+let reverseStringSplit = (str) => {
+// reverse string with reverse and join method
+// first we should convert it to an array
+  newStr = str.split('').reverse().join('')
+  console.log(newStr)
+}
+
+reverseStringSplit("bye");
